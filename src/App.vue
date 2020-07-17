b435dab4cbf647e109c272bcda5d3aaf4fa05525
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-07-17 19:44:45 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="d-flex flex-column" id="app">
+    <Navbar />
+    <router-view class="flex-grow" />
+    <Footer class="mt-auto"/>
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Footer,
+    Navbar
+  }
+};
+</script>
 
 <style>
 #app {
@@ -15,6 +24,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100vh;
 }
 
 #nav {
